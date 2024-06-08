eaf50669e937b42da4d2457c82fe87465d43a17e
yashmishra201023 <mishrayash2402@gmail.com>
2024-06-08 15:42:14 +0530

    Add files via upload

---
+++ organise.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+
+
+from_dir = "C:/Users/Harishchandra mishra/Downloads/"
+to_dir = "C:/Users/Harishchandra mishra/Downloads/Stellar stage 7"
+
+list_of_files = os.listdir(from_dir)
+
+
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+   
+
+    if extension == '':
+        continue
+    if extension in ['.pdf']:
+
+        path1 = from_dir + '/' + file_name                       
+        path2 = to_dir + '/' + "Image_Files"                     
+        path3 = to_dir + '/' + "Image_Files" + '/' + file_name  
+       
+        if os.path.exists(path2):
+          print("Moving " + file_name + ".....")
+
+          
+          shutil.move(path1, path3)
+
+        else:
+          os.makedirs(path2)
+          print("Moving " + file_name + ".....")
+          shutil.move(path1, path3)
